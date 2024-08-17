92e7038d9fe8c1af38cb9b5ef18dc24798301b73
Mike Tinnes <mtinnes@gmail.com>
2024-08-16 21:03:51 -0500

    update doc generation

--- src/interfaces/options.interface.ts
+++ src/interfaces/options.interface.ts
@@ -1,3 +1,12 @@
+/**
+ * @interface
+ */
 export interface IOptions {
+
+    /**
+     * Use legacy event handling
+     * 
+     * @type {boolean}
+     */
     useLegacyEventHandling?: boolean;
 }
